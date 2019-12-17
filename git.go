dc038a54999a75411b731fc3dcee24e335832f34
thisisaaronland <devnull@localhost>
2019-12-17 10:56:29 -0800

    first cut at native Git indexer

--- git.go
+++ git.go
@@ -3,12 +3,10 @@ package git
 import (
 	"context"
 	"github.com/whosonfirst/go-whosonfirst-index"
-	"github.com/whosonfirst/go-whosonfirst-index/fs"
 	gogit "gopkg.in/src-d/go-git.v4"
-	_ "gopkg.in/src-d/go-git.v4/plumbing/protocol/packp/sideband"
-	"io/ioutil"
-	"os"
-	"path/filepath"
+	"gopkg.in/src-d/go-git.v4/plumbing/object"
+	"gopkg.in/src-d/go-git.v4/storage/memory"
+	
 )
 
 func init() {
@@ -16,58 +14,52 @@ func init() {
 	index.Register("git", dr)
 }
 
+type GitDriver struct {
+	index.Driver
+}
+
 func NewGitDriver() index.Driver {
 
-	rd := fs.NewRepoDriver()
-
-	dr := &GitDriver{
-		repo_driver: rd,
-	}
+	dr := &GitDriver{}
 
 	return dr
 }
 
-type GitDriver struct {
-	index.Driver
-	repo_driver index.Driver
-}
-
 func (d *GitDriver) Open(uri string) error {
-	return d.repo_driver.Open(uri)
+
+	return nil
 }
 
 func (d *GitDriver) IndexURI(ctx context.Context, index_cb index.IndexerFunc, uri string) error {
 
-	repo_name := filepath.Base(uri)
-
-	tempdir, err := ioutil.TempDir("", repo_name)
-
-	if err != nil {
-		return err
-	}
-
-	defer os.RemoveAll(tempdir)
-
-	/*
-		pr := &WOFLoggerProgress{
-			logger: i.Logger,
-		}
-	*/
-
-	// something something something auth-y bits
-	// https://godoc.org/gopkg.in/src-d/go-git.v4#CloneOptions
-
 	opts := &gogit.CloneOptions{
-		URL:   uri,
-		Depth: 1,
-		// Progress: pr,
+		URL: uri,
 	}
-
-	_, err = gogit.PlainClone(tempdir, false, opts)
+	
+	r, err := gogit.Clone(memory.NewStorage(), nil, opts)
 
 	if err != nil {
 		return err
 	}
 
-	return d.repo_driver.IndexURI(ctx, index_cb, tempdir)
+	it, err := r.BlobObjects()
+
+	if err != nil {
+		return err
+	}
+
+	err = it.ForEach(func(bl *object.Blob) error {
+
+		fh, err := bl.Reader()
+
+		if err != nil {
+			return err
+		}
+
+		defer fh.Close()
+
+		return index_cb(ctx, fh)
+	})
+
+	return nil
 }
