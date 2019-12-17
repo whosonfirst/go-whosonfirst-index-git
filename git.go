7ce74fa097fcbebfd49c147d28401bfc61e638b4
thisisaaronland <devnull@localhost>
2019-12-17 12:17:48 -0800

    update to do everything in Git

--- git.go
+++ git.go
@@ -6,7 +6,9 @@ import (
 	gogit "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing/object"
 	"gopkg.in/src-d/go-git.v4/storage/memory"
-	
+	"net/url"
+	"os"
+	"path/filepath"
 )
 
 func init() {
@@ -16,41 +18,98 @@ func init() {
 
 type GitDriver struct {
 	index.Driver
+	target string
+	preserve bool
 }
 
 func NewGitDriver() index.Driver {
-
 	dr := &GitDriver{}
-
 	return dr
 }
 
 func (d *GitDriver) Open(uri string) error {
 
+	u, err := url.Parse(uri)
+
+	if err != nil {
+		return err
+	}
+
+	d.target = u.Path
+
+	q := u.Query()
+
+	if q.Get("preserve") == "1" {
+		d.preserve = true
+	}
+	
 	return nil
 }
 
 func (d *GitDriver) IndexURI(ctx context.Context, index_cb index.IndexerFunc, uri string) error {
 
+	var repo *gogit.Repository
+	
 	opts := &gogit.CloneOptions{
 		URL: uri,
 	}
+
+	switch d.target {
+	case "":
 	
-	r, err := gogit.Clone(memory.NewStorage(), nil, opts)
+		r, err := gogit.Clone(memory.NewStorage(), nil, opts)
+		
+		if err != nil {
+			return err
+		}
+
+		repo = r
+	default:
+
+		fname := filepath.Base(uri)
+		path := filepath.Join(d.target, fname)
+		
+		r, err := gogit.PlainClone(path, false, opts)
+
+		if err != nil {
+			return err
+		}
+
+		if !d.preserve {
+			defer os.RemoveAll(path)
+		}
+		
+		repo = r
+	}
+	
+	ref, err := repo.Head()
 
 	if err != nil {
 		return err
 	}
 
-	it, err := r.BlobObjects()
+	commit, err := repo.CommitObject(ref.Hash())
 
 	if err != nil {
 		return err
 	}
 
-	err = it.ForEach(func(bl *object.Blob) error {
+	tree, err := commit.Tree()
 
-		fh, err := bl.Reader()
+	if err != nil {
+		return err
+	}
+
+	tree.Files().ForEach(func(f *object.File) error {
+
+		switch filepath.Ext(f.Name) {
+		case ".geojson":
+			// continue
+		default:
+			return nil
+		}
+
+		fh, err := f.Reader()
 
 		if err != nil {
 			return err
@@ -58,6 +117,7 @@ func (d *GitDriver) IndexURI(ctx context.Context, index_cb index.IndexerFunc, ur
 
 		defer fh.Close()
 
+		ctx := index.AssignPathContext(ctx, f.Name)
 		return index_cb(ctx, fh)
 	})
 
